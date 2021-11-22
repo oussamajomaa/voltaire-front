230a546269241b4d497d6362e90a3f2835d744cc
oussamajomaa <osm70@gmx.com>
2021-11-23 00:07:21 +0100

    hum1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 
 export const environment = {
   production: false,
-  // url:'https://cchum-kvm-obvil.in2p3.fr:3000',
-  url:'http://vps783302.ovh.net:3000',
+  url:'https://cchum-kvm-obvil.in2p3.fr:3000',
+  // url:'http://vps783302.ovh.net:3000',
   // url:'http://localhost:3000',
   // url:'https://obtic.sorbonne-universite.fr:3000',
 };
