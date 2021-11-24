89d8cd4d673689ca57075a2ed283376c80c6b8f5
oussamajomaa <osm70@gmx.com>
2021-11-24 09:15:54 +0100

    url heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  url:'https://cchum-kvm-obvil.in2p3.fr:3000',
+  url:"https://obvil-voltaire.herokuapp.com/"
+  // url:'https://cchum-kvm-obvil.in2p3.fr:3000',
   // url:'http://vps783302.ovh.net:3000',
   // url:'https://obtic.sorbonne-universite.fr:3000',
 };
